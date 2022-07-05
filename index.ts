70a6d7b1f50be4b86acce10ab53286aaa1d24f06
RoAlencar <roalencarx@gmail.com>
2022-07-05 16:46:07 -0300

    Como funcionam as interfaces

--- index.ts
+++ index.ts
@@ -1,3 +1,34 @@
 function soma(a: number, b: number){
     return a + b;
 }
+
+
+//types
+
+//interfaces
+
+interface IAnimal {
+    
+    nome: string;
+    tipo: 'terrestre' | 'aquático';
+    executarRugido(alturaEmDecibeis: number): void;
+}
+
+interface IFelino extends IAnimal {
+    visaoNoturna: boolean;
+}
+
+const animal: IAnimal = {
+    nome: 'Elefante',
+    tipo: "terrestre",
+    executarRugido: (alturaEmDecibeis) => (`$(alturaEmDecibeis)`)
+}
+
+animal.executarRugido('s')
+
+const felino: IFelino = {
+    nome: 'Leão',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+    
+}
